f5e838edb57fddb170f10ee24e13af61eb1418d2
Sujit R. Chhetri <sujitchhetri@gmail.com>
2019-03-26 19:07:25 -0700

    Update graphlightweight.py

--- src/code2graph/core/graphlightweight.py
+++ src/code2graph/core/graphlightweight.py
@@ -48,17 +48,20 @@ class CallVisitor(ast.NodeVisitor):
         self.function_to_be_visited=[]
 
     def visit_Call(self, node):
-
+        # print("Entered visit Call!")
         call_name = astor.to_source(node.func).strip()
         base_name = call_name.split('.')[-1]
-
+        # print("\n Node:", node)
         # print("finding call full name: %s, base name: %s" % (call_name, base_name))
         # print(self.root)
         proj_function_found = False
         matched = False
+        flag_check = False
 
         if "pyan" in self.root:
+            # print("Inside Pyan!!")
             for cand in self.call_graph_visitor.uses_edges[self.root["pyan"]]:
+                # print("Inside Pyan:", cand)
                 if isinstance(cand.ast_node, ast.FunctionDef):
                     if cand.name == base_name:
                         # print (cand)
@@ -78,28 +81,40 @@ class CallVisitor(ast.NodeVisitor):
                         self.function_to_be_visited.append((cand, new_node))
 
                         proj_function_found = True
+                elif isinstance(cand.ast_node, ast.FunctionDef):
+                    pass
+            flag_check = True
 
         if not proj_function_found and '.' in call_name:
-
+            # print("inside Call name .!!")
             result = self.type_manager.fuzzy_search(call_name)
+            # print("\ncall_name:", call_name)
+            # print("match list:", result)
+
 
             if result:
                 matching = self.type_manager.type_hash[result[0]]
                 # print(call_name, matching)
                 new_node = {"name":matching['name'].split('.')[-1], "url":matching['url'], "children":[], "type":"tf_keyword"}
 
-                self.root['children'].append(new_node)
+                new_node["args"] = []
+                # self.root['children'].append(new_node)
 
                 if len(node.args):
                     for idx, arg in enumerate(node.args):
+
                         another_visitor = CallVisitor(self.call_graph_visitor, new_node)
                         another_visitor.visit(arg)
 
-                        new_node["args"] = []
+
                         if isinstance(arg, ast.Str):
                             new_node["args"].append(arg.s)
                         elif isinstance(arg, ast.Num):
                             new_node["args"].append(arg.n)
+                            # print("idx:", idx, "args:", arg.n)
+                        elif isinstance(arg, ast.Tuple):
+                            new_node["args"].append(astor.to_source(arg).strip())
+                            # print("idx:", idx, "args:", astor.to_source(arg).strip())
 
                 if len(node.keywords):
                     for keyword in node.keywords:
@@ -109,29 +124,66 @@ class CallVisitor(ast.NodeVisitor):
                         new_node["keywords"] = {}
                         if isinstance(keyword.value, ast.Str):
                             new_node["keywords"][str(keyword.arg)] = keyword.value.s
+                            # print("--match with .: Keyword", keyword.value.s)
                         elif isinstance(keyword.value, ast.Num):
                             new_node["keywords"][str(keyword.arg)] = keyword.value.n
+                            # print("--match with .: Keyword", keyword.value.n)
                         elif isinstance(keyword.value, ast.Attribute):
                             new_node["keywords"][str(keyword.arg)] = astor.to_source(keyword.value).strip()
+                            # print("--match with .: Keyword", astor.to_source(keyword.value).strip())
                         elif isinstance(keyword.value, ast.Tuple):
                             new_node["keywords"][str(keyword.arg)] = astor.to_source(keyword.value).strip()
+                            # print("--match with .: Keyword", astor.to_source(keyword.value).strip())
+
+                self.root['children'].append(new_node)
+
                 matched = True
+            flag_check = True
 
         if not matched:
+            # print("Inside Not matched!")
+
             if len(node.args):
                 for arg in node.args:
+                    # print("\targs:", arg)
                     another_visitor = CallVisitor(self.call_graph_visitor, self.root)
                     another_visitor.visit(arg)
 
                     self.function_to_be_visited += another_visitor.function_to_be_visited
 
             if len(node.keywords):
-                for keyword in node.keywords:
-                    another_visitor = CallVisitor(self.call_graph_visitor, self.root)
-                    another_visitor.visit(keyword)
+                for i,keyword in enumerate(node.keywords):
+                    # print("key #:%d keyword name: %s  "%(i, keyword.arg))
+                    # print("keyword class:",keyword.__class__.__name__)
+                    for field, value in ast.iter_fields(keyword):
+                        # print("fieldd:",field, "value:",value)
+                        if  isinstance(value, ast.Call):
+                            another_visitor_k = CallVisitor(self.call_graph_visitor, self.root)
+                            another_visitor_k.visit(keyword)
+                            self.function_to_be_visited += another_visitor_k.function_to_be_visited
+                        elif isinstance(value, ast.Str):
+                            result = self.type_manager.fuzzy_search(value.s)
+                            if result:
+                                matching = self.type_manager.type_hash[result[0]]
+                                new_node = {"name": matching['name'].split('.')[-1], "url": matching['url'],
+                                            "children": [], "args":[],"type": "tf_keyword"}
 
-                    self.function_to_be_visited += another_visitor.function_to_be_visited
+                                self.root['children'].append(new_node)
 
+                        elif isinstance(value, list):
+                            for item in value:
+                                if isinstance(item, ast.Str):
+                                    result = self.type_manager.fuzzy_search(item.s)
+                                    if result:
+                                        matching = self.type_manager.type_hash[result[0]]
+                                        new_node = {"name": matching['name'].split('.')[-1], "url": matching['url'],
+                                                    "children": [], "args" : [], "type": "tf_keyword"}
+                                        self.root['children'].append(new_node)
+            flag_check = True
+
+        # if not flag_check:
+        #     print("Didn't go through anything!!!!")
+        # print("Existing visit call \n")
 
 class ProgramLineVisitor:
 
@@ -146,7 +198,7 @@ class ProgramLineVisitor:
             return
 
         for child in ast.iter_child_nodes(node):
-
+            # print("parent:", parent, "child:",child)
             if isinstance(child, (ast.Expr, ast.UnaryOp, ast.withitem, ast.Assign, ast.Compare, \
                                   ast.AugAssign, ast.Return, ast.Call, ast.Assert)):
 
@@ -160,7 +212,10 @@ class ProgramLineVisitor:
             elif isinstance(child, (ast.If, ast.Try, ast.ExceptHandler, ast.For, ast.With)):
                 self.visit(child, parent)
 
-            elif isinstance(child, (ast.FunctionDef, ast.arguments)):
+            elif isinstance(child, ast.FunctionDef):
+                pass
+            elif isinstance(child, ast.arguments):
+                # node["args"].append(child.n)
                 pass
 
 class TFTokenExplorer:
@@ -171,6 +226,8 @@ class TFTokenExplorer:
 
         # mapping utilities
         self.call_graph_visitor = CallGraphVisitor(glob("%s/**/*.py" % str(code_repo_path), recursive=True))
+        # for n in self.call_graph_visitor.uses_edges:
+        #     print(n)
         self.pyan_node_dict = {} # hashmap from RDF node name to pyan node.
 
         # generated in build the one complete call graph
@@ -183,7 +240,7 @@ class TFTokenExplorer:
         self.rdf_graphs = {}
 
         # generated in build tfseqeuences
-        self.tfseqeuences = {}
+        self.tfsequences = {}
 
     def explore_code_repository(self):
 
@@ -197,22 +254,26 @@ class TFTokenExplorer:
 
     def build_call_graph(self):
         for caller in self.call_graph_visitor.uses_edges:
+
             if caller.flavor is not Flavor.UNSPECIFIED and caller.flavor is not Flavor.UNKNOWN:
 
                 self.call_graph.add((BNode(get_name(caller)), OntologyManager.is_type, BNode(caller.flavor)))
                 self.pyan_node_dict[get_name(caller)]=caller
 
                 for callee in self.call_graph_visitor.uses_edges[caller]:
+                    # print(caller, callee)
                     if callee.flavor is not Flavor.UNSPECIFIED and callee.flavor is not Flavor.UNKNOWN:
                         self.call_graph.add((BNode(get_name(callee)), OntologyManager.is_type, BNode(callee.flavor)))
                         self.pyan_node_dict[get_name(callee)]=callee
 
                         self.call_graph.add((BNode(get_name(caller)), OntologyManager.call, BNode(get_name(callee))))
+        # self.dump_call_graph()
 
     def build_call_trees(self):
         roots = self.find_roots(self.call_graph)
-
+        # print("roots:",roots)
         for root in roots:
+            # print("roots:", root)
             call_tree = self.grow_function_calls(root)
 
             call_tree["rdf_name"]= call_tree["name"]
@@ -240,7 +301,7 @@ class TFTokenExplorer:
 
     def grow_function_calls(self, start):
 
-        print("Processing node: %s" % str(start))
+        # print("Processing node: %s" % str(start))
         line_visitor = ProgramLineVisitor(self.call_graph_visitor, self.pyan_node_dict[start])
         line_visitor.visit(self.pyan_node_dict[start].ast_node, line_visitor.root)
 
@@ -270,13 +331,20 @@ class TFTokenExplorer:
         if "children" in node:
             for idx, child in enumerate(node["children"]):
                 graph.add((BNode(node['rdf_name']), OntologyManager.call, BNode(child["rdf_name"])))
-                # if idx > 0:
-                    # graph.add((BNode(node["children"][idx-1]['rdf_name']), BNode("followed_by"), BNode(child["rdf_name"])))
+                if idx > 0:
+                    graph.add((BNode(node["children"][idx-1]['rdf_name']), BNode("followed_by"), BNode(child["rdf_name"])))
                 self.build_rdf_graph(child, graph)
 
         if "args" in node:
-            for idx, arg in enumerate(node['args']):
-                graph.add((BNode(node['rdf_name']), BNode("has_arg%d"%idx), BNode((arg))))
+            # print("\n Node:---->",node, node['args'])
+            if len(node['args'])==3:
+                k_size="("+str(node['args'][1])+","+str(node['args'][2])+")"
+                graph.add((BNode(node['rdf_name']), BNode("has_output_feature_size"), BNode((node['args'][0]))))
+                graph.add((BNode(node['rdf_name']), BNode("has_kernel_size"), BNode(k_size)))
+            else:
+                for idx, arg in enumerate(node['args']):
+                    # print(arg)
+                    graph.add((BNode(node['rdf_name']), BNode("has_arg%d"%idx), BNode((arg))))
 
         if "keywords" in node:
             for keyword in node['keywords']:
@@ -286,7 +354,7 @@ class TFTokenExplorer:
         for root in self.call_trees:
             sequence = []
             self.build_tfsequence(self.call_trees[root], sequence)
-            self.tfseqeuences[root] = sequence
+            self.tfsequences[root] = sequence
 
     def build_tfsequence(self, node, sequence):
 
@@ -307,8 +375,8 @@ class TFTokenExplorer:
 
         # self.dump_call_graph()
         # self.dump_call_trees()
-        # self.dump_rdf_graphs()
-        self.dump_tfseuqences()
+        self.dump_rdf_graphs()
+        # self.dump_tfsequences()
 
     def dump_call_graph(self):
         self.pyvis_draw(self.call_graph, str(self.code_repo_path/"call_graph"))
@@ -324,8 +392,8 @@ class TFTokenExplorer:
         for root in self.rdf_graphs:
             self.pyvis_draw(self.rdf_graphs[root], str(self.code_repo_path/root.replace('.','')))
 
-    def dump_tfseuqences(self):
-        pprint.pprint(self.tfseqeuences)
+    def dump_tfsequences(self):
+        pprint.pprint(self.tfsequences)
 
     def pyvis_draw(self, graph, name):
 
@@ -334,6 +402,7 @@ class TFTokenExplorer:
         G= Network(height="800px", width="70%", directed=True)
 
         for src, edge, dst in graph:
+            print(src,edge,dst)
 
             if edge == OntologyManager.is_type:
                 continue
@@ -380,18 +449,7 @@ def lightweight(path):
 
 if __name__ == "__main__":
 
-
-    # path = Path(".")/"test"/"FewShot_GAN-Unet3D-master"
-    # path = Path(".")/"test"/"adaptive-f-divergence-master"/'bnn'
-    # path = Path("/Users/louisccc/Dropbox/louis_research/development/darpa/DARPA-DCC/data/data_paperswithcode/09/NPRF-master") # python 2
-    # path = Path("/Users/louisccc/Dropbox/louis_research/development/darpa/DARPA-DCC/data/data_paperswithcode/23/KGD-master") # lambda problem
-    # path = Path("/Users/louisccc/Dropbox/louis_research/development/darpa/DARPA-DCC/data/data_paperswithcode/24/FewShot_GAN-Unet3D-master")
-    # path = Path("/Users/louisccc/Dropbox/louis_research/development/darpa/DARPA-DCC/data/data_paperswithcode/31/RecurJac-Jacobian-Bounds-master") # remove parse_landscape.py
-    # path = Path("/Users/louisccc/Dropbox/louis_research/development/darpa/DARPA-DCC/data/data_paperswithcode/34/enas-master") # python 2 
-    # path = Path("/Users/louisccc/Dropbox/louis_research/development/darpa/DARPA-DCC/data/data_paperswithcode/37/Micro-Net-master") # ok
-    # path = Path("/Users/louisccc/Dropbox/louis_research/development/darpa/DARPA-DCC/data/data_paperswithcode/42/AdverseBiNet-master") # ok
-    # path = Path("/Users/louisccc/Dropbox/louis_research/development/darpa/DARPA-DCC/data/data_paperswithcode/43/T-GANs-master") # python 2
-    # path = Path("/Users/louisccc/Dropbox/louis_research/development/darpa/DARPA-DCC/data/data_paperswithcode/49/ambient-gan-master") # python 2
-    path = Path(".")/"test"/"fashion_mnist"
+    path = Path(".") / "test" / "fashion_mnist"
+    # path = Path(".")/"test"/"VGG16"
     path = path.resolve()
     lightweight(path)
