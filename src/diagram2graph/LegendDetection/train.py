f7c267ee8bfd591c29648ffea6472dd36f29977b
Adismns <44849408+Adismns@users.noreply.github.com>
2019-07-30 14:02:23 -0400

    Create train.py

---
+++ src/diagram2graph/LegendDetection/train.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+#%matplotlib inline
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+from torchvision import datasets,transforms
+import argparse,os
