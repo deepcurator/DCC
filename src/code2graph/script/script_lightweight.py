e07bd492c948ba7b51585b58bd62150cae72a9c1
Aung <aung@pop-os.localdomain>
2019-07-30 11:51:04 -0700

    move files for opt 1

--- src/code2graph/script/script_lightweight.py
+++ src/code2graph/script/script_lightweight.py
@@ -154,7 +154,7 @@ def move_output_files(config: LightWeightMethodConfig):
         else:
             copy_files(config.input_path, config.dest_path, "*.triples")
             
-    if 3 in config.output_types:
+    if 3 in config.output_types or 1 in config.output_types:
         copy_files(config.input_path, config.dest_path, "*.html")
     
     if 6 in config.output_types:
