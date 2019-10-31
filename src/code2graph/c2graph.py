ccd1946e28573c3f71bded23acadfafc6df9258d
kulatu00 <tugba.kulahcioglu@siemens.com>
2019-10-31 04:36:11 -0400

    updated to process all new html files in the code repository

--- src/code2graph/c2graph.py
+++ src/code2graph/c2graph.py
@@ -1,14 +1,12 @@
 import os
 from code2graph.script import script_lightweight
 import shlex
-
-# The following command runs the script on input code repository and generates an RDF graph in turtle format.
-# The generated RDF graph file is placed inside the input code repository path.
-
-
+import glob
 
 def run(code_repository_path, outputFolder):
 
+    existing_html_files = glob.glob(code_repository_path + '/*.html')
+    
     c2g_output_dir = outputFolder + "\code2graph"
 
     if not os.path.exists(c2g_output_dir):
@@ -23,28 +21,26 @@ def run(code_repository_path, outputFolder):
     script_lightweight.pipeline_the_lightweight_approach(argv[2:])
     
     try:
-        os.remove(c2g_output_dir + "/code2graph.rdf")
+        os.remove(c2g_output_dir + "/code2graph.ttl")
     except:
         pass
+        
+    all_html_files = glob.glob(code_repository_path + '/*.html')
+        
+    for html_file in all_html_files:
+        if html_file not in existing_html_files:
+            html_file_name = os.path.basename(html_file)
+            try:
+                os.remove(c2g_output_dir + html_file_name)
+            except:
+                pass
+                
+            os.rename(html_file, c2g_output_dir + "/" + html_file_name)
     
-    try:
-        os.remove(c2g_output_dir + "/main.html")
-    except:
-        pass
-        
-    try:
-        os.remove(c2g_output_dir + "/metadata.html")
-    except:
-        pass
-        
-    try:
-        os.remove(c2g_output_dir + "/generate_cifar10_tfrecords.html")
-    except:
-        pass
 
-    os.rename(code_repository_path + "/rdf_graph.rdf", c2g_output_dir + "/code2graph.rdf")
-    os.rename(code_repository_path + "/main.html", c2g_output_dir + "/main.html")
-    os.rename(code_repository_path + "/metadata.html", c2g_output_dir + "/metadata.html")
-    os.rename(code_repository_path + "/generate_cifar10_tfrecords.html", c2g_output_dir + "/generate_cifar10_tfrecords.html")
+    os.rename(code_repository_path + "/rdf_graph.rdf", c2g_output_dir + "/code2graph.ttl")
+    # os.rename(code_repository_path + "/main.html", c2g_output_dir + "/main.html")
+    # os.rename(code_repository_path + "/metadata.html", c2g_output_dir + "/metadata.html")
+    # os.rename(code_repository_path + "/generate_cifar10_tfrecords.html", c2g_output_dir + "/generate_cifar10_tfrecords.html")
     
     print("[Info] Completed code2graph pipeline!")
