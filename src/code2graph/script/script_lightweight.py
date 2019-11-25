86c543907df6fbb71b85a7866d26bc37b69bfe42
Aung <aung@pop-os.localdomain>
2019-11-24 17:55:29 -0800

    bug fix

--- src/code2graph/script/script_lightweight.py
+++ src/code2graph/script/script_lightweight.py
@@ -208,13 +208,13 @@ def move_output_files(config: LightWeightMethodConfig):
         copy_files(config.input_path, config.dest_path, "*.rdf")
 
 
-def copy_files(data_path, dest_path, filetype, name_index=3):
+def copy_files(data_path, dest_path, filetype, name_index=2):
     """ Copy files in data_path that matches filetype to dest_path """
     for path in Path(data_path).rglob(filetype):
         path = Path(path)
-
+        code_dir_path = path
         for _ in range(name_index):
-            code_dir_path = path.parent
+            code_dir_path = code_dir_path.parent
 
         repo_path = Path(dest_path) / code_dir_path.name
         if not repo_path.is_dir():
