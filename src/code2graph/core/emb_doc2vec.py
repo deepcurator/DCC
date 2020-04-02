9def819c0222bdef972537bb7d7dc53fc191bc9f
louisccc <louis29418401@gmail.com>
2020-04-02 10:39:15 -0700

    clean doc2vec pipeline.

--- src/code2graph/core/emb_doc2vec.py
+++ src/code2graph/core/emb_doc2vec.py
@@ -2,27 +2,6 @@ import gensim
 import collections
 import numpy as np
 from pathlib import Path
-from sklearn.model_selection import train_test_split
-
-
-def save_processed_data(data, path):
-    with open(path, 'w') as f:
-        for line in data:
-            f.write(line)
-
-
-def preprocess(raw_data_path: Path, filename, save_path: Path):
-    
-    functions = [] 
-
-    for file in raw_data_path.rglob(filename):
-        with open(file, 'r') as f:
-            for line in f:
-                functions.append(line)
-
-    train, test = train_test_split(functions, test_size=0.1)
-    save_processed_data(train, str(save_path / "train.txt"))
-    save_processed_data(test, str(save_path / "test.txt"))
 
 
 class Doc2Vec:
@@ -42,12 +21,17 @@ class Doc2Vec:
 
     def read_dataset(self, fname, tokens_only=False):
         with open(fname, encoding="iso-8859-1") as f:
-            for i, line in enumerate(f):
+            for line in f:
+                splited_line = line.split(" ")
+                
+                tag = splited_line[0]
+                content = " ".join(splited_line[1:])
+
                 if tokens_only:
-                    yield gensim.utils.simple_preprocess(line)
+                    yield tag, gensim.utils.simple_preprocess(content)
                 else:
                     # For training data, add tags
-                    yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [i])
+                    yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(content), [tag])
 
     def train_model(self, vector_size=50, window=2, min_count=2, epochs=40, workers=4):
         self.model = gensim.models.doc2vec.Doc2Vec(vector_size=vector_size, window=window, 
@@ -74,6 +58,16 @@ class Doc2Vec:
         sims = self.model.docvecs.most_similar([vector], topn=len(self.model.docvecs))
         return ' '.join(self.train_corpus[sims[0][0]].words)
 
+    def evaluate(self):
+        for tag, test_corpus in self.test_corpus:
+            print("evaluating", tag, test_corpus)
+
+            inferred_vector = self.model.infer_vector(test_corpus)
+            sims = self.model.docvecs.most_similar([inferred_vector], topn=len(self.model.docvecs))
+
+            print(sims)
+            print(inferred_vector)
+
     def save_model(self, fname):
         save_path = self.data_dir / fname
         self.model.save(str(save_path))
@@ -81,20 +75,3 @@ class Doc2Vec:
     def load_model(self, fname):
         load_path = self.data_dir / fname
         self.model = gensim.models.doc2vec.Doc2Vec.load(str(load_path))
-
-if __name__ == "__main__":
-
-    raw_data_path = Path("../graphast_output").resolve()
-    save_path = Path("../doc2vec").resolve()
-    save_path.mkdir(exist_ok=True)
-    # preprocess(raw_data_path, "functions.txt", save_path)
-    
-    d2v = Doc2Vec(save_path)
-    # d2v.train_model()
-    # d2v.save_model("d2v_model")
-    # d2v.load_model("d2v_model")
-    # test = [] 
-    # with open(str(save_path / "test.txt"), "r") as file:
-    #     for line in file:
-    #         test.append(line)
-    # import pdb; pdb.set_trace()
---
+++ src/code2graph/script/emb/script_train_doc2vec.py
@@ -0,0 +1,67 @@
+import gensim
+import collections
+import numpy as np
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+import sys
+
+sys.path.append('../..')
+
+from core.emb_doc2vec import Doc2Vec
+
+def prepare_dataset(dataset_path: str, filename):
+    
+    raw_data_path = Path(dataset_path).resolve()
+    dataset_save_path = Path("../doc2vec").resolve()
+    dataset_save_path.mkdir(exist_ok=True)
+    
+    data_functions = [] 
+
+    for code2vec_file in raw_data_path.rglob(filename):
+        with open(code2vec_file, 'r') as file:
+            for function_line in file:
+                splited_function_line = function_line.split(" ")
+                labels = splited_function_line[0]
+                content = " ".join(splited_function_line[1:])
+                data_functions.append((labels, content))
+
+    train, test = train_test_split(data_functions, test_size=0.1, shuffle=True)
+
+    with open(str(dataset_save_path / "train.txt"), 'w') as file:
+        for labels, content in train:
+            file.write(labels)
+            file.write(" ")
+            file.write(content)
+
+    with open(str(dataset_save_path / "test.txt"), 'w') as file:
+        for labels, content in test:
+            file.write(labels)
+            file.write(" ")
+            file.write(content)
+
+    return dataset_save_path
+
+def run_doc2vec(dataset_path:str):
+       
+    dataset_save_path = prepare_dataset(dataset_path, "doc2vec.txt")
+
+    d2v = Doc2Vec(dataset_save_path)
+    d2v.train_model()
+    d2v.save_model("d2v_model")
+
+    d2v.load_model("d2v_model")
+    d2v.dump_vectors()
+    d2v.evaluate()
+    
+    import pdb; pdb.set_trace()
+
+if __name__ == "__main__":
+    run_doc2vec('../../graphast_output')
+
+    # d2v.train_model()
+    # d2v.save_model("d2v_model")
+    # d2v.load_model("d2v_model")
+    # test = [] 
+    # with open(str(save_path / "test.txt"), "r") as file:
+    #     for line in file:
+    #         test.append(line)
