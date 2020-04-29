ec107a02970151690a542307561aa2f1cba48fe4
kulatu00 <tugba.kulahcioglu@siemens.com>
2020-04-29 09:28:15 -0400

    cleanup

--- demo/run_all_modalities/visualize.py
+++ demo/run_all_modalities/visualize.py
@@ -16,36 +16,6 @@ import matplotlib.pyplot as plt
 # cnames = ['blue', 'green', 'red', 'cyan', 'orange', 'black', 'purple', 'yellow', 'navy', "brown", "gray", "magenta", "#ee55ff", 'blue', 'green', 'red', 'cyan', 'orange', 'black', 'purple', 'yellow', 'navy', "brown", "gray", "magenta", "#ee55ff"]
 cnames = ['#a569bd', '#ec7063', '#5dade2', '#58d68d', '#f4d03f', '#bfc9ca', '#d0efff', '#2a9df4', '#45b6fe', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573', '#566573']
 
-auto_color_map_text = {}
-auto_color_map_text["https://cso.kmi.open.ac.uk/"] = cnames[0]
-auto_color_map_text["https://github.com/deepcurator/DCC/Eval"] = cnames[7]
-auto_color_map_text["https://github.com/deepcurator/DCC/Generic"] = cnames[2]
-auto_color_map_text["https://github.com/deepcurator/DCC/Publication"] = cnames[1]
-auto_color_map_text["https://github.com/deepcurator/DCC/Method"] = cnames[3]
-auto_color_map_text["https://github.com/deepcurator/DCC/Task"] = cnames[4]
-auto_color_map_text["https://github.com/deepcurator/DCC/Other"] = cnames[5]
-auto_color_map_text["https://github.com/deepcurator/DCC/Material"] = cnames[6]
-auto_color_map_text["Literal"] = cnames[5]
-
-auto_color_map_image = {}
-auto_color_map_image["https://cso.kmi.open.ac.uk/"] = cnames[0]
-auto_color_map_image["https://github.com/deepcurator/DCC/NormBlock"] = cnames[3]
-auto_color_map_image["https://github.com/deepcurator/DCC/Figure"] = cnames[1]
-auto_color_map_image["https://github.com/deepcurator/DCC/RnnBlock"] = cnames[2]
-auto_color_map_image["Literal"] = cnames[5]
-
-auto_color_map_code = {}
-auto_color_map_code["https://cso.kmi.open.ac.uk/"] = cnames[0]
-auto_color_map_code["https://github.com/deepcurator/DCC/UserDefined"] = cnames[4]
-auto_color_map_code["https://github.com/deepcurator/DCC/tf"] = cnames[3]
-auto_color_map_code["Literal"] = cnames[5]
-
-auto_color_map_ont = {}
-auto_color_map_text["https://github.com/deepcurator/DCC/Figure"] = cnames[1]
-auto_color_map_text["https://github.com/deepcurator/DCC/RnnBlock"] = cnames[2]
-auto_color_map_text["https://github.com/deepcurator/DCC/NormBlock"] = cnames[3]
-
-
 auto_color_map_merged = {}
 auto_color_map_merged["https://cso.kmi.open.ac.uk/"] = cnames[0]
 auto_color_map_merged["https://github.com/deepcurator/DCC/Eval"] = "#a1513b"
@@ -62,7 +32,6 @@ auto_color_map_merged["https://github.com/deepcurator/DCC/UserDefined"] = cnames
 auto_color_map_merged["https://github.com/deepcurator/DCC/tf"] = cnames[3]
 auto_color_map_merged["Literal"] = cnames[5]
 auto_color_map_merged["Text"] = "#83de1b"
-
 
 defaults = []
 
@@ -84,8 +53,7 @@ def addNode(G, src, dst, edge, src_type, dst_type, src_text, dst_text, src_title
     edge_width = 0.5
     dash = False
     if str(edge) == "https://github.com/deepcurator/DCC/followedBy":
-        dash = True
-        
+        dash = True     
     
     if color_map == None:
         if src_type not in auto_color_map:
@@ -127,29 +95,8 @@ def get_vis(graph, modality, max_node_count=10000, rels=[], show=True, color_map
     # ont.parse("run_all_modalities/DeepSciKG.nt", format="ttl")
             
     G = Network(height="500px", width="100%", directed=directed, notebook=notebook)
-    # G.repulsion()
-    # G.barnes_hut(gravity=-20000, spring_length=50) # no parameter, use 50 node size
-    # G.inherit_edge_colors(True)
-    
-    # G.toggle_physics(False)
-    # G.toggle_stabilization(False)
-    # G.show_buttons(filter_=['physics'])
-    
     
     auto_color_map = auto_color_map_merged
-    
-    #if modality == "Text":
-     #     auto_color_map = auto_color_map_text
-    #elif modality == "Image":
-     #   auto_color_map = auto_color_map_image
-    #elif modality == "Schema":
-     #   auto_color_map = auto_color_map_ont
-    #elif modality == "Code":
-     #   auto_color_map = auto_color_map_code
-    #elif modality == "Merged":
-     #   auto_color_map = auto_color_map_merged
-      #  G.show_buttons(filter_=['physics'])
-    
     if  modality == "Merged":
         G.show_buttons(filter_=['physics'])
     
@@ -171,10 +118,6 @@ def get_vis(graph, modality, max_node_count=10000, rels=[], show=True, color_map
         src_type = [x for x in graph[src:RDF.type]]
         dst_type = [x for x in graph[dst:RDF.type]]
         
-        # print(str(src) +"\t" + str(src_type))
-        # print(str(dst) +"\t" + str(dst_type))
-        # print()
-        
         if modality == "Image" and src_type == []:
             if "_Comp" in str(src):
                 src_type = "Component"
@@ -186,12 +129,7 @@ def get_vis(graph, modality, max_node_count=10000, rels=[], show=True, color_map
                 dst_type = "https://github.com/deepcurator/DCC/Component"
             elif "_Text" in str(dst):
                 dst_type = "https://github.com/deepcurator/DCC/Text"
-        
-        
-        # a = URIRef("https://github.com/deepcurator/DCC/CondenseNet.txt_method")
-        
-        # for s,p,o in g.triples((None, b, None)):
-          #  print(s,p,o)
+
         src_text = str(src)
         for s,p,o in graph.triples((src, hasText, None)):
             # print(s,p,o)
@@ -205,8 +143,7 @@ def get_vis(graph, modality, max_node_count=10000, rels=[], show=True, color_map
         dst_text = dst_text.replace("https://github.com/deepcurator/DCC/", "")
         
         src_text = src_text.replace("http://www.w3.org/2002/07/owl", "")
-        dst_text = dst_text.replace("http://www.w3.org/2002/07/owl", "")
-        
+        dst_text = dst_text.replace("http://www.w3.org/2002/07/owl", "")    
         
         if len(src_type) > 0:
             src_type = str(src_type[0])
@@ -217,22 +154,13 @@ def get_vis(graph, modality, max_node_count=10000, rels=[], show=True, color_map
                 dst_type = "OWL"
                 continue
             elif str(src).startswith("https://github.com/deepcurator/DCC/"):
-                # print("--------------------" + src)
-                # src_type = "DCC"
                 src_type = str(src)
             elif src_type == []:
                 src_type = "Literal"
                 continue
             else:
-                # blank node
                 # src_type = "blank node"
-                
-                # pass
                 continue
-                
-                # r = Resource(graph, src)
-                # for predicates, objects in graph.predicate_objects(subject=src):
-                   #  print (predicates + "\t" + objects)
         if len(dst_type) > 0:
             dst_type = str(dst_type[0])
         else:
@@ -250,11 +178,7 @@ def get_vis(graph, modality, max_node_count=10000, rels=[], show=True, color_map
                 dst_type = "Literal"
                 # continue
             else:
-                # dst_type = "default"
-                # blank node
                 # dst_type = "blank node"
-                
-                # pass
                 continue
         
         src_title = str(src)
@@ -293,12 +217,6 @@ def get_vis(graph, modality, max_node_count=10000, rels=[], show=True, color_map
             dst_type = "https://github.com/deepcurator/DCC/tf"
         elif "_Text" in str(dst):
             dst_type = "Text"
-        
-        # print("src\t" + str(src) + "\t" + str(src_type))
-        # print("edge\t" + str(edge))
-        # print("dst\t" + str(dst) + "\t" + str(dst_type))
-        # print("")
-        
        
         src_text = src_text.replace(".txt", "")
         dst_text = dst_text.replace(".txt", "")
@@ -306,7 +224,6 @@ def get_vis(graph, modality, max_node_count=10000, rels=[], show=True, color_map
         dst = str(dst).replace(".txt", "")
         src_title = src_title.replace(".txt", "")
         dst_title = dst_title.replace(".txt", "")
-        
         
         if len(rels) > 0:
             if (show) and (str(edge) in rels):
@@ -318,16 +235,5 @@ def get_vis(graph, modality, max_node_count=10000, rels=[], show=True, color_map
         if count == max_node_count:
             break  
         count += 1
-    
-    # print("Triple count: " + str(count))
-    
-    # print()
-    # for node_type, color in auto_color_map.items():
-        # print(node_type + "\t" + color)
-    # print()
-    # for rel in rel_list:
-        # print(rel)
-    
-    # print(auto_color_map)
     
     return G
